8ba02affc3f5eab60fb04e92b77a5432dcc826ad
Offiongwhilley <offiongwhilley@gmail.com>
2022-07-19 10:13:12 -0700

    correction

--- sort.h
+++ sort.h
@@ -1,7 +1,9 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef __SORT__H
+#define __SORT__H
 
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,19 +19,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
+int partition(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
-void _qsort(int *a, int low, int high, int size);
-void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-#endif
+void quick(int *array, int start, int end, size_t size);
+
+#endif /*__SORT__H*/
