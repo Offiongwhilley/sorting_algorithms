0975fd9d98e2e4cd245225b943c2b126ed863c3e
Offiongwhilley <offiongwhilley@gmail.com>
2022-07-19 02:35:13 -0700

    correction sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,9 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef _SORTING_H_
+#define _SORTING_H_
+#include <stdio.h>
 
-#include <stdlib.h>
+
+/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,19 +19,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/** Given FUN prototype */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
